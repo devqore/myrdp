ad93a1b42118ff20efb241b291afbbf91dfbbf1d
szatanszmatan <szatanszmatan@gmail.com>
2017-08-22 21:55:57 +0200

    build release improvements

--- freeze.sh
+++ freeze.sh
@@ -2,4 +2,9 @@
 rm build/ -rf
 rm dist/ -rf
 . /opt/python2-venvs/myrdp/bin/activate
+export VERSION=`python2 -c "import app; print app.__version__"`
+export VERSIONED_NAME=myrdp-$VERSION
 pyinstaller myrdp.spec
+cd dist
+mv myrdp $VERSIONED_NAME
+tar -jcvf $VERSIONED_NAME.tbz2 $VERSIONED_NAME
